11f335e372f7dadcba1bec4296168722cb89c309
oookurtoo <stepan.fedyk@gmail.com>
2019-10-26 23:02:38 +0300

    force upd

--- tictactoe/service-worker.js
+++ tictactoe/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.001d6f142bb6f1f32dd419f686232438.js"
+  "/tictactoe/precache-manifest.c7ef09f6d9820c3481c77a9b2be1b39c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tictactoe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
