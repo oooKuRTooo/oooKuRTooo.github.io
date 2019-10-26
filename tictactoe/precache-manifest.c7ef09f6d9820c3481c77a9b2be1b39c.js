d138a22758a81ce1761a2794b5186c77aa006aa5
oookurtoo <stepan.fedyk@gmail.com>
2019-10-26 23:22:49 +0300

    updt

---
+++ tictactoe/precache-manifest.c7ef09f6d9820c3481c77a9b2be1b39c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af270ef981049c17a24b523525373958",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "fc5b500f2b795dad23d3",
+    "url": "/tictactoe/static/css/main.2ce1af35.chunk.css"
+  },
+  {
+    "revision": "6a3168280eb7c1affa90",
+    "url": "/tictactoe/static/js/2.040e9aa3.chunk.js"
+  },
+  {
+    "revision": "fc5b500f2b795dad23d3",
+    "url": "/tictactoe/static/js/main.a9d7573d.chunk.js"
+  },
+  {
+    "revision": "1dd1878a39f419eb6036",
+    "url": "/tictactoe/static/js/runtime-main.868a150c.js"
+  }
+]);
