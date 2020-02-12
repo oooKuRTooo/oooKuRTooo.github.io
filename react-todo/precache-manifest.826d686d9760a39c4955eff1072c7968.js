9ae8ac9d73c409731af0396c6121823725a9ddfc
oookurtooo <stepan.fedyk@gmail.com>
2020-02-12 15:04:23 +0200

    Create simple todo list on React

---
+++ react-todo/precache-manifest.826d686d9760a39c4955eff1072c7968.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b989cb2766c3bf9b2950313ea90528a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb18185b5997734b6d5d",
+    "url": "/static/css/main.dfd83887.chunk.css"
+  },
+  {
+    "revision": "837d2d0642b41868f213",
+    "url": "/static/js/2.a33b94f8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.a33b94f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb18185b5997734b6d5d",
+    "url": "/static/js/main.76127b19.chunk.js"
+  },
+  {
+    "revision": "d84a3e19d8a947dff9dd",
+    "url": "/static/js/runtime-main.b2044548.js"
+  }
+]);
